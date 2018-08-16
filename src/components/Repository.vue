86b8c123651d0a73cd69cd5f20236b0331c8b4cf
Paco Moreno <pc.moreno2099@gmail.com>
2018-08-16 12:31:49 -0500

    Basis for TAL detail, still needs some work

--- src/components/Repository.vue
+++ src/components/Repository.vue
@@ -1,13 +1,38 @@
 <template>
     <div class="container mt-2 mx-4">
       <h1>{{ $t('repositories.title') }} {{ this.$route.params.talId }}</h1>
+      <p>{{ tal }}</p>
+      <p v-if="error">{{ error }}</p>
+      <b-button @click="back">{{ $t('general.return') }}</b-button>
     </div>
 </template>
 
 <script>
+import axios from '@/axios'
+import config from '@/config'
+
 export default {
   data () {
-    return {}
+    return {
+      eventHub: null,
+      tal: null,
+      error: null
+    }
+  },
+  methods: {
+    back () {
+      this.$router.back()
+    },
+    successCb (response) {
+      this.tal = response.data
+    },
+    errorCb (error) {
+      this.error = error
+    }
+  },
+  created: function () {
+    const service = config.api.services.get.talDetail.replace(':id', this.$route.params.talId)
+    axios.get(this.$root.$i18n.locale, service, this.successCb, this.errorCb, this.eventHub)
   }
 }
 </script>
