d134bea93f7a6b7914f90080b79b94ed69abf642
Paco Moreno <pc.moreno2099@gmail.com>
2018-10-11 11:12:50 -0500

    Organize table content

--- src/components/common/CustomTable.vue
+++ src/components/common/CustomTable.vue
@@ -1,67 +1,79 @@
 <template>
-  <b-container>
-    <loading :show="loading"></loading>
-    <error-display :error="error"></error-display>
-    <b-row align-h="between">
-      <b-col cols="6">
-        <b-form-group horizontal>
-          <b-input-group>
-            <b-input-group-prepend>
-              <b-form-select id="searchFilter" v-model="searchFilter">
-                <option value=null>
-                  {{ $t('filter.all') }}
-                </option>
-                <option v-for="opt in searchFilterOpts" :key="opt.value" :value="opt.value">
-                  {{ $t(opt.text) }}
-                </option>
-              </b-form-select>
-            </b-input-group-prepend>
-            <b-form-input v-model="filterItem" :placeholder="$t('filter.placeholder')"></b-form-input>
-            <b-input-group-append>
-              <b-btn :disabled="!customFilter" @click="filterItem = ''" variant="outline-primary">{{ $t('filter.clean') }}</b-btn>
-            </b-input-group-append>
-          </b-input-group>
-        </b-form-group>
-      </b-col>
-      <b-col cols="6">
-        <b-form-group horizontal label-for="maxPerPage" :label="$t('general.maxResults')">
-          <b-form-select id="maxPerPage" v-model="perPage" :options="resultsPerPageOpts"></b-form-select>
-        </b-form-group>
+  <b-container fluid>
+    <b-row align-h="center">
+      <b-col cols="10">
+        <loading :show="loading"></loading>
       </b-col>
     </b-row>
-    <b-table striped hover responsive show-empty
-      :id="tableId"
-      :items="items"
-      :fields="tableFields"
-      :per-page="perPage"
-      :total-rows="totalRows"
-      :current-page="tableCurrentPage"
-      :filter="customFilter"
-      @filtered="onFiltered"
-      :empty-filtered-text="$t('errors.noDataFound')"
-      no-provider-paging
-      no-provider-sorting
-      no-provider-filtering>
-      <template v-for="field in tableFields" :slot="'HEAD_' + field.key" slot-scope="data">
-        {{ $t(data.label) }}
-      </template>
-      <template v-if="showDetailButton || showDeleteButton" slot="action" slot-scope="row">
-        <b-button size="sm" class="mr-2" :to="{path: ''+row.item.id}" append v-if="showDetailButton">
-          {{ $t('general.showDetail') }}
-        </b-button>
-        <b-button size="sm" class="mr-2" variant="outline-danger" @click="deleteCallback(row.item)" append v-if="showDeleteButton">
-          {{ $t('common.delete') }}
-        </b-button>
-      </template>
-      <template slot="table-caption">
-        <p v-if="totalRows > perPage">
-          {{ $t('general.displaying', {from: fromResult, to: toResult, total: totalRows}) }}
-        </p>
-      </template>
-    </b-table>
-    <b-pagination v-if="totalRows > perPage" size="md" :total-rows="totalRows"
-                  :per-page="perPage" v-model="tableCurrentPage" align="center">
-    </b-pagination>
+    <b-row align-h="center">
+      <b-col cols="10">
+        <error-display :error="error"></error-display>
+      </b-col>
+    </b-row>
+    <b-row align-h="between">
+      <b-col cols="5" align-self="start">
+        <b-input-group>
+          <b-input-group-prepend>
+            <b-form-select id="searchFilter" v-model="searchFilter">
+              <option value=null>
+                {{ $t('filter.all') }}
+              </option>
+              <option v-for="opt in searchFilterOpts" :key="opt.value" :value="opt.value">
+                {{ $t(opt.text) }}
+              </option>
+            </b-form-select>
+          </b-input-group-prepend>
+          <b-form-input v-model="filterItem" :placeholder="$t('filter.placeholder')"></b-form-input>
+          <b-input-group-append>
+            <b-btn :disabled="!customFilter" @click="filterItem = ''" variant="outline-primary">{{ $t('filter.clean') }}</b-btn>
+          </b-input-group-append>
+        </b-input-group>
+      </b-col>
+      <b-col cols="2">
+        <b-form-select id="maxPerPage" v-model="perPage" :options="resultsPerPageOpts">
+          <template slot="first">
+            <option :value="null" disabled>-- {{ $t('general.maxResults') }} --</option>
+          </template>
+        </b-form-select>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="12">
+        <b-table striped hover responsive show-empty
+          :id="tableId"
+          :items="items"
+          :fields="tableFields"
+          :per-page="perPage"
+          :total-rows="totalRows"
+          :current-page="tableCurrentPage"
+          :filter="customFilter"
+          @filtered="onFiltered"
+          :empty-filtered-text="$t('errors.noDataFound')"
+          no-provider-paging
+          no-provider-sorting
+          no-provider-filtering>
+          <template v-for="field in tableFields" :slot="'HEAD_' + field.key" slot-scope="data">
+            {{ $t(data.label) }}
+          </template>
+          <template v-if="showDetailButton || showDeleteButton" slot="action" slot-scope="row">
+            <b-button size="sm" class="mr-2" :to="{path: ''+row.item.id}" append v-if="showDetailButton">
+              {{ $t('general.showDetail') }}
+            </b-button>
+            <b-button size="sm" class="mr-2" variant="outline-danger" @click="deleteCallback(row.item)" append v-if="showDeleteButton">
+              {{ $t('common.delete') }}
+            </b-button>
+          </template>
+          <template slot="table-caption">
+            <p v-if="totalRows > perPage">
+              {{ $t('general.displaying', {from: fromResult, to: toResult, total: totalRows}) }}
+            </p>
+          </template>
+        </b-table>
+        <b-pagination v-if="totalRows > perPage" size="md" :total-rows="totalRows"
+                      :per-page="perPage" v-model="tableCurrentPage" align="center">
+        </b-pagination>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
